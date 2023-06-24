8b5e65214970ad8c5d7f0d5cfc521e1cd0d2d57e
HatemGw <aassdd127zzee@gmail.com>
2023-06-24 14:41:24 +0200

    dd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+//colors in local storage
+let mainColors = localStorage.getItem("color-options");
+
+if(mainColors !== null){
+    document.documentElement.style.setProperty('--second-background-color', mainColors);
+
+    document.querySelectorAll('.setting-box .colors li').forEach(Element=>{
+        Element.classList.remove('active');
+    if(Element.dataset.color === mainColors){
+        Element.classList.add('active');
+    }
+    });
+}
+
 const colorsLi = document.querySelectorAll('.setting-box .option-box .colors li');
 
 //changing colors in settings-box
@@ -5,9 +19,20 @@ colorsLi.forEach(li =>{
     
     li.addEventListener('click',(e)=>{
         
-     console.log(e.target.dataset.color);
-        document.documentElement.style.setProperty('--second-background-color', e.target.dataset.color)
+    document.documentElement.style.setProperty('--second-background-color', e.target.dataset.color)
+    //store in local storage
+    localStorage.setItem('color-options', e.target.dataset.color);
+
+    //remove active class from other li
+    e.target.parentElement.querySelectorAll('.active').forEach(Element =>{
+        Element.classList.remove('active');
+    })
+
+    //add active class for target element
+    e.target.classList.add('active');
     } )
+
+    
 })
 
 //open settings-box
